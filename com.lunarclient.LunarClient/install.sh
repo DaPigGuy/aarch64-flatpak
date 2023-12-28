7c89439989d56c71820c880dbb10ceec668bbe6f
DaPigGuy <mcpepig123@gmail.com>
2023-12-28 15:58:02 -0800

    lunarclient: Include app metainfo

--- com.lunarclient.LunarClient/install.sh
+++ com.lunarclient.LunarClient/install.sh
@@ -2,6 +2,7 @@ mv squashfs-root/resources/extracted_asar/launcher-linux-arm64/ /app/lunarclient
 
 # Install app contents
 install lunarclient "/${FLATPAK_DEST}/bin"
+install -Dm644 com.lunarclient.LunarClient.metainfo.xml "${FLATPAK_DEST}/share/metainfo/${FLATPAK_ID}.metainfo.xml"
 install -Dm644 com.lunarclient.LunarClient.desktop "${FLATPAK_DEST}/share/applications/${FLATPAK_ID}.desktop"
 install -Dm644 com.lunarclient.LunarClient.png ${FLATPAK_DEST}/share/icons/hicolor/512x512/apps/lunarclient.png
 
